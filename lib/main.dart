4add506dc0a51921c77b3a7ca4797bc35aa626a7
tsunoda <tsunoda@aomori-u.ac.jp>
2022-10-22 15:36:17 +0900

    実行時エラー対応

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
